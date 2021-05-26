25434de841e204d88b41fdea83db6e08e30d57c4
TeslaNex <teslanexx@gmail.com>
2021-05-26 15:59:25 -0300

    Es un ejemplo

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
  * File:   main.cpp
  * Author: Patricia
  *
- * Created on 26 de mayo de 2021, 15:47
+ * Created on 26 de mayo de 2021, 12:37
  */
-
+#include<stdio.h>
 #include <cstdlib>
 
 using namespace std;
@@ -18,8 +18,25 @@ using namespace std;
 /*
  * 
  */
-int main(int argc, char** argv) {
 
+void sumar (int,int);
+int x;
+int *p=&x;
+int main(void) 
+{
+
+    printf("Hola Mundo\n");
+    sumar(2,3);
+    printf("Resultado:%d",*p);
+    
+    
     return 0;
 }
+
+void sumar (int t,int b)
+{
+*p=(t+b);
+}
+
+
 
